31dd815b20ea1b5d8dbaf5ca79cb0ccdbcf10640
bessa_ <claudioafbessa@gmail.com>
2024-11-28 00:26:45 +0000

    feat: link main page

---
+++ public/links.ts
@@ -0,0 +1,20 @@
+export const links = [
+  {
+    caption: 'Github',
+    url: 'https://github.com/assebc',
+    icon: '/github.png',
+    width: '32'
+  },
+  {
+    caption: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/claudio-bessa/',
+    icon: '/linkedin.png',
+    width: '36'
+  },
+  {
+    caption: 'Email',
+    url: 'mailto:bessaprofessional@gmail.com',
+    icon: '/email.png',
+    width: '36'
+  }
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { links } from '../../public/links';
+import { CommonModule } from '@angular/common';
+
+interface Link {
+  caption: string;
+  url: string;
+  icon?: string;
+  width?: string;
+}
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bessa';
+  readonly links: Link[] = links;
 }
