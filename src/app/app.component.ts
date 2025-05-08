ecf5ad17136d4434c919e4be9ed27f29f422d282
assebc <claudioafbessa@gmail.com>
2025-05-08 09:48:54 +0100

    fix title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { Router, RouterModule, RoutesRecognized } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     imports: [RouterModule],
     template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly router: Router, private readonly titleService: Title) {
+    this.setDocumentTitle();
+  }
+    
+  private setDocumentTitle(): void {
+    this.router.events.subscribe((route) => {
+      if (!(route instanceof RoutesRecognized)) { return; } // prettier-ignore
+      if (route.url.includes('prompt')){
+        this.titleService.setTitle('Prompt Engineering');
+      }
+    });
+  }
+}
